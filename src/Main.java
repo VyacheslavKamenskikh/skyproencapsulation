dd0ef497d52ef0e4048aed6ac9f8c44c90bc36e6
KLOZ-Z <ya.slava2323yaru@gmail.com>
2023-01-15 14:42:46 +0300

    Hometask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nTask 2");
+        Car carWithConstructor = new Car("BMW","Z8",3.0,"черный",2021,"Германия","АКПП","родстер","к775нп77",2,false);
+        System.out.println(carWithConstructor.toString());
+        carWithConstructor.changeTires(1);
+        System.out.println(carWithConstructor.toString());
     }
 }
